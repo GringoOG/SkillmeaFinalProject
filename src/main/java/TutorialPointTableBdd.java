2b5f3b521a9e77aa47e124b2f57e3cbc5ead0c0f
Gringo Gang <miroslav.pecek1@gmail.com>
2025-09-09 19:03:46 +0200

    Finish test and add cucumber

--- src/main/java/TutorialPointTableBdd.java
+++ src/main/java/TutorialPointTableBdd.java
@@ -1,17 +1,50 @@
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TutorialPointTableBdd {
-    @Given("uzivatel se nachazi na strance {string}")
-    public void uzivatelSeNachaziNaStrance(String arg0) {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
     }
 
-    @Then("jmeno na druhem radku tavbulky bude {string}")
-    public void jmenoNaDruhemRadkuTavbulkyBude(String arg0) {
+    @After
+    public void cleanUp() {
+        driver.close(); //u firefoxu problem, kdyz jsou oba prikazy. Nechat jen .quit
+        driver.quit();
+    }
+
+
+
+
+    @Given("uzivatel se nachazi na strance {string}")
+    public void uzivatelSeNachaziNaStrance(String url) {
+        driver.get(url);
+    }
+
+    @Then("jmeno na druhem radku tabulky bude {string}")
+    public void jmenoNaDruhemRadkuTavbulkyBude(String jmeno) {
+        String name = driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[2]/td[1]")).getText();
+        assertEquals(jmeno, name);
+
     }
 
     @And("prijmeni na druhem radku tabulky bude {string}")
-    public void prijmeniNaDruhemRadkuTabulkyBude(String arg0) {
+    public void prijmeniNaDruhemRadkuTabulkyBude(String prijmeni) {
+        String lastName = driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[2]/td[2]")).getText();
+        assertEquals(prijmeni, lastName);
+
     }
 }
--- src/main/resources/TutorialPointTable.feature
+++ src/main/resources/TutorialPointTable.feature
@@ -2,5 +2,5 @@ Feature: TutorialPointTable
 
   Scenario: Overeni dat tabulky
     Given uzivatel se nachazi na strance "https://www.tutorialspoint.com/selenium/practice/webtables.php#"
-    Then jmeno na druhem radku tavbulky bude "Alden"
+    Then jmeno na druhem radku tabulky bude "Alden"
     And prijmeni na druhem radku tabulky bude "Cantrell"
